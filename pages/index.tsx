d9ea3b657ad26308f5c27062b06ef934b3ac297f
Tony-Poomipartes <tony.p.pro@gmail.com>
2023-06-30 15:27:45 +0200

    ✨ feat:start the index

---
+++ pages/index.tsx
@@ -0,0 +1,66 @@
+import type { NextPage } from 'next';
+
+import Song from '../components/Song/Song';
+import Audio from '../components/Audio/Audio';
+import styles from '../styles/Home.module.css';
+import { type } from 'os';
+
+type Song = {
+id: number;
+title:string;
+artist: string;
+file: string;
+image: string;
+}
+
+
+
+const SONGS: Song[] = [
+  {
+    id: 0,
+    title: 'Birdseye Blues',
+    artist: 'Chris Haugen',
+    file: 'Birdseye Blues - Chris Haugen.mp3',
+    image: '/1.jpg'
+  },
+  {
+    id: 1,
+    title: 'Depth Fuse',
+    artist: 'French Fuse',
+    file: 'Depth Fuse - French Fuse.mp3',
+    image: '/2.jpg'
+  },
+  {
+    id: 2,
+    title: 'Duh Fuse',
+    artist: 'French Fuse',
+    file: 'Duh Fuse - French Fuse.mp3',
+    image: '/3.jpg'
+  }
+]
+
+export const getStaticProps = async()=>{
+  const allSongs:Song[] = SONGS;
+  return {
+    props:{
+      song: allSongs
+    },
+    revalidate: 3600
+  }
+}
+
+
+const Home: NextPage<{ songs: Song[]}> = ({ songs }) =>{
+  const [trackPlaying, setTrackPlaying] = useState<number>(0)
+  const [isPlaying, setIsPlaying] = useState<boolean>(false);
+  return (
+    <div className={styles.container}>
+      <div className={styles.songPlaying}>
+        <Song isPlaying={isPlaying} trackPlaying={trackPlaying} song={songs[trackPlaying]} />
+      </div>
+      <Audio isPlaying={isPlaying} setIsPlaying={setIsPlaying} songs={songs} trackPlaying={trackPlaying} setTrackPlaying={setTrackPlaying} />
+    </div>
+  )
+}
+
+export default Home
