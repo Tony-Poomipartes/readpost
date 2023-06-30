f922a862c0397a27b4065446c46f7e3840aea985
Tony-Poomipartes <tony.p.pro@gmail.com>
2023-06-30 16:38:40 +0200

    ✨ feat: add song.tsx + 💄  style:

---
+++ components/Song/Song.tsx
@@ -0,0 +1,22 @@
+import classes from 'components/Song/Song.module.css'
+import Image from 'next/image';
+
+type Song = {
+  id: number;
+  title: string;
+  artist: string;
+  file: string;
+  image: string
+}
+
+const Song = (props: {song: Song, trackPlaying: number, isPlaying: boolean}) => {
+  return (
+    <>
+      <div>
+        <Image className={props.isPlaying ? classes.turning : classes.notTurning} src={props.song.image} width={300} height={300}/>
+      </div>
+    </>
+  )
+}
+
+export default Song;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
 import type { NextPage } from 'next';
-
+import { useState } from 'react';
 import Song from '../components/Song/Song';
-import Audio from '../components/Audio/Audio';
-import styles from '../styles/Home.module.css';
+// import Audio from '../components/Audio/Audio';
+import styles from 'styles/home.module.css';
 import { type } from 'os';
 
 type Song = {
@@ -56,7 +56,7 @@ const Home: NextPage<{ songs: Song[]}> = ({ songs }) =>{
   return (
     <div className={styles.container}>
       <div className={styles.songPlaying}>
-        <Song isPlaying={isPlaying} trackPlaying={trackPlaying} song={songs[trackPlaying]} />
+        <Song songs={songs[trackPlaying]} isPlaying={isPlaying} trackPlaying={trackPlaying} song={songs[trackPlaying]} />
       </div>
       <Audio isPlaying={isPlaying} setIsPlaying={setIsPlaying} songs={songs} trackPlaying={trackPlaying} setTrackPlaying={setTrackPlaying} />
     </div>
